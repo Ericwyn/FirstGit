370abbc2857a812b8ff1c935ea23ce5de8838f75
Ericwyn <Ericwyn.chen@gmail.com>
2017-01-07 12:15:15 +0800

    修改了Main.java文件，新增了一个A类和B类

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
 public class Main{
 	public static void main(String [] args){
-		System.out.println("Hello world");
+		B b=new B(134,12,"hello",1245532);
+		System.out.println(B.name);
+	}
+}
+class A {
+	int height;
+	int age;
+	String name;
+	public A(int height,int age,String name){
+		this.height=height;
+		this.age=age;
+		this.name=name;
+	}
+}
+
+class B extends A{
+	int ID;
+	public B(int height,int age,String name,int ID){
+		super(height,age,name);
+		this.ID=ID;
 	}
 }
